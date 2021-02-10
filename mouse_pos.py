6c2a02ceaef5e246bd4ff34f296bf91c6042a417
mohammad <mhmoslemi2338@.com>
2021-02-10 13:47:48 +0330

    adding mouse tracker

---
+++ mouse_pos.py
@@ -0,0 +1,22 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame as pg
+
+def mouse_pos(k,x=768,y=576):
+    pg.init()
+    screen = pg.display.set_mode((x, y))
+    clicks = []
+    done=False 
+    i=0
+    while not done:
+        for event in pg.event.get():
+            #if event.type == pg.QUIT:
+            if i==k:
+                done=True
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                clicks.append(event.pos)
+                i+=1
+        screen.fill((30, 30, 30))
+        pg.display.flip()
+    pg.quit()
+    return clicks
